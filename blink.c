fece3e3d8b803f994142ce75303287e1455ef720
Zheng Hao Tan <tanzhao@umich.edu>
2015-10-13 20:11:43 -0400

    initial commit; makefile needs to be fixed

---
+++ blink.c
@@ -0,0 +1,20 @@
+#include  <msp430x20x3.h>
+
+int main(void)
+{
+	WDTCTL = WDTPW + WDTHOLD;
+
+	// Set the direction of the pin.
+	P1DIR |= 0x01;
+
+	volatile unsigned long i;
+
+	for (;;)
+	{
+		// Toggle the pin.
+		P1OUT ^= 0x01;
+
+		i = 0;
+		while (i++ != 99999);
+	}
+}
